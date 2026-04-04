a31c4e41d096020103a46b6538594278743178a0
Max Kinet <maxkinet@gmail.com>
2026-04-03 20:44:56 -0700

    add tile texture

--- src/prefabs/Tile.js
+++ src/prefabs/Tile.js
@@ -2,20 +2,14 @@ import {globals} from '../main.js'
 import {types} from '../main.js'
 
 export class Tile extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y) {
-        super(scene, x, y);
+    constructor(scene, x, y, texture) {
+        super(scene, x, y, texture);
         this.scene = scene;
         this.type = types.empty;
+        this.setDisplaySize(globals.width / 4, globals.height / 4);
+        this.setOrigin(0);
+        scene.add.existing(this);
 
-        // Create the tile
-        this.tex = this.scene.add.rectangle(
-            x, y,
-            globals.width / globals.grid_size,
-            globals.height / globals.grid_size,
-            0xff00ff
-        ).setOrigin(0);
-        this.tex.setStrokeStyle(2);
-        
     }
 
     
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,12 +4,18 @@ import {Tile} from '../prefabs/Tile.js'
 
 export class Play extends Phaser.Scene {
 
+    preload() {
+        this.load.setPath('./assets');
+        
+        this.load.image('empty', 'empty.png');
+    }
+
     create() {
         // Fill play area with empty tiles
         let size = globals.width / globals.grid_size;
         for (let i = 0; i < globals.grid_size; i++) {
             for (let j = 0; j < globals.grid_size; j++) {
-                new Tile(this, i * size, j * size);
+                new Tile(this, i * size, j * size, 'empty');
             }
         }
 
