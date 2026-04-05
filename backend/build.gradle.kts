109b7b1c22a2b9a8e64cb788d31c3c6a420a1cd8
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-04-04 18:52:53 -0700

    Add OpenAPI spec for automated generation of REST api, begin REST controller for the Game State

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,13 +1,23 @@
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
 plugins {
     id("java")
     id("idea")
     id("org.springframework.boot") version "4.0.5"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.openapi.generator") version "7.21.0"
 }
 
 group = "us.drullk"
 version = "1.0-SNAPSHOT"
 java.toolchain.languageVersion = JavaLanguageVersion.of(25)
+
+val openApiSpecPath = "$rootDir/src/main/resources/tictactoe-api.yml"
+val openApiGeneratePath = "$rootDir/tictactoe-server-api"
+
+sourceSets.main {
+    java.srcDir("${openApiGeneratePath}/src/main/java")
+}
 
 repositories {
     mavenCentral()
@@ -22,4 +32,47 @@ dependencies {
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("io.swagger.core.v3:swagger-annotations:2.2.46")
+    implementation("io.swagger:swagger-annotations:1.6.16")
+}
+
+// https://www.thewokecoder.io/generate-controller-interfaces-with-gradle/ (Caveat: Article shows Groovy not Kotlin script)
+tasks.register<GenerateTask>("generateGameServerApi") {
+    // Sets for gradle sync
+    inputs.file(openApiSpecPath)
+    outputs.dir(openApiGeneratePath)
+
+    // https://github.com/OpenAPITools/openapi-generator/blob/master/modules/openapi-generator-gradle-plugin/README.adoc#openapigenerate
+    this.generatorName.set("spring")
+    this.inputSpec.set(openApiSpecPath)
+    this.outputDir.set(openApiGeneratePath)
+    this.apiPackage.set("us.drullk.api")
+    this.modelPackage.set("us.drullk.model")
+    this.invokerPackage.set("us.drullk.invoker")
+    this.configOptions.set(
+        mapOf(
+            // https://openapi-generator.tech/docs/configuration/
+            "generateApis" to "true",
+            "generateModels" to "true",
+            // https://openapi-generator.tech/docs/generators/spring/
+            "interfaceOnly" to "true",
+            "library" to "spring-boot",
+            "openApiNullable" to "false",
+            "useBeanValidation" to "true",
+            "useSpringBoot4" to "true",
+        )
+    )
+}
+
+tasks.compileJava {
+    dependsOn("generateGameServerApi")
+}
+
+tasks.classes {
+    dependsOn("generateGameServerApi")
+}
+
+tasks.clean {
+    delete("$rootDir/tictactoe-server-api")
 }
--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -1,1 +1,5 @@
 rootProject.name = "tictactoe"
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
